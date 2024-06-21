33465edf5c190e6788f8e4dc9bf59e5716ef0f2b
canetizen <mcancaliskan@outlook.com>
2024-06-21 22:44:19 +0300

    giris

--- c0_giris/c0_0.py
+++ c0_giris/c0_0.py
@@ -1,17 +1,18 @@
-# Interpretation/Compilation Example
-# Mustafa Can Caliskan
-# ITU Supercomputer Club, 2024
+"""
+    Interpretation/Compilation Example
+    Author: Mustafa Can Caliskan
+    ITU Supercomputer Club, 2024
+"""
 
-def print_hpc():
-    print("Hello, I'm running!")
+def print_language():
+    print("Hello, I'm Python!")
 
 def do_sth_wrong():
-    ptr = None
-    ptr[0] = 42
+    x
 
 if __name__ == "__main__":
-    print_hpc()
+    print_language()
 
     do_sth_wrong()  # This function will intentionally do things wrong.
 
-    print_hpc()
+    print_language()
--- c0_giris/c0_1.c
+++ c0_giris/c0_1.c
@@ -1,13 +1,14 @@
 /*
-    Basic C Code Structure
-    Mustafa Can Caliskan
-    ITU Supercomputer Club, 2024
+ * Basic C Code Structure
+ * Author: Mustafa Can Caliskan
+ * ITU Supercomputer Club, 2024 
 */
+
 #include <stdio.h> // Standard I/O library
 
 int main(int argc, char** argv) {
 
     printf("Hello, HPC!\n"); // The function used to output text to the terminal
 
-    return 0;
+    return 0; // The value of 0 indicates that the program has executed successfully and completed.
 }
